c4b1b77f583d847224e6d92b5bf75c8f8a4ac8fa
Pradeep Daka <pradeepkumardaka@sbpcorp.in>
2023-11-07 09:28:07 +0000

    Initial Commit

---
+++ webapp/model/formatter.js
@@ -0,0 +1,11 @@
+sap.ui.define([], function () {
+    "use strict";
+
+    return {
+        liveChange:function(oLive){
+            this.fireLiveChange();
+        // return (oLive);
+        }
+    };
+
+});
